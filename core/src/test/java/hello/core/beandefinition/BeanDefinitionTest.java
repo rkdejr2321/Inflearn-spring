8295bf12d9822c3ecf50b6f6ceab655e2070ed13
river_duck <rkdejr2321@naver.com>
2022-02-18 23:45:41 +0900

    2022-02-18-code update

--- core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -22,7 +22,6 @@ public class BeanDefinitionTest {
                 System.out.println("beanDefinitionName = " + beanDefinitionName +
                         " beanDefinition = " + beanDefinition);
             }
-
         }
     }
 }
