efcd7b3acea140b09823457532e088b0c1d161c4
rkdejr2321 <rkdejr2321@naver.com>
2023-01-02 21:35:14 +0900

    [FATCH] 회원 이름으로 조회 쿼리문 오타 수정

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -30,7 +30,7 @@ public class MemberRepository {
     }
 
     public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where Member.name = :name", Member.class)
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", name)
                 .getResultList();
     }
